217c8b21db0a9a7e4c81cd69c01e72cc6f553609
wguerreroc1969 <wguerreroc1969@gmail.com>
2026-04-27 10:30:57 -0500

    Arreglo a gitignore

--- Guerrero_William_prueba_rpa/config.py
+++ Guerrero_William_prueba_rpa/config.py
@@ -1,12 +1,23 @@
 import os
 from dotenv import load_dotenv
-
+# Busca un archivo llamado .env en el directorio actual y carga las variables de entorno definidas en él.   
 load_dotenv()
 
+# Busca la variable "API_URL" en el entorno del sistema operativo.
+# Si existe → la usa
+# Si NO existe → usa el valor por defecto max espera 5 segundos para la respuesta de la API
 API_URL = os.getenv("API_URL", "https://jsonplaceholder.typicode.com/todos/")
-TIMEOUT = 5
+TIMEOUT = 5 
+
+# Ruta del archivo de entrada
+
+# ✔ Busca en .env
+# ✔ Si no existe → usa "data/ordenes.xlsx"
 
 INPUT_FILE = os.getenv("INPUT_FILE", "data/ordenes.xlsx")
 OUTPUT_FILE = os.getenv("OUTPUT_FILE", "data/reporte_final.xlsx")
 
+# Define la ruta del archivo de logs
+# ✔ No usa .env (valor fijo)
+# ✔ Guarda mensajes del sistema (errores, ejecución, etc.)
 LOG_FILE = "logs/proceso.log"
